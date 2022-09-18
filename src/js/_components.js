4a8284872b3a9a9817cfb4893c0853860587da41
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-09-18 15:44:37 +0300

    done=>dev[tutorials]

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import aos from "./components/aos.js";
 import faq from "./components/faq.js";
 import slider from "./components/slider.js";
 import lessons from "./components/lessons.js";
+import tutorials from "./components/tutorials.js";
